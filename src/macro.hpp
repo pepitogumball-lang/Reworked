c114e7c711c1ba356428306f937c3fe70335c487
pepitogumball-lang <pepitogumball@gmail.com>
2026-04-23 09:20:57 -0700

    fix: remove duplicate input struct from macro.hpp

--- src/macro.hpp
+++ src/macro.hpp
@@ -21,21 +21,6 @@ enum state {
     playing
 };
 
-struct input : gdr::Input {
-    input() = default;
-
-    input(int frame, int button, bool player2, bool down, uint8_t subFrameIndex = 0)
-        : Input(frame, button, player2, down, subFrameIndex) {}
-
-    bool operator==(const input& other) const {
-        return frame == other.frame &&
-               player2 == other.player2 &&
-               button == other.button &&
-               down == other.down &&
-               subFrameIndex == other.subFrameIndex;
-    }
-};
-
 struct Macro : gdr::Replay<Macro, input> {
 
     Macro() : Replay("Reworked", reworkedVersion.c_str()) {}
